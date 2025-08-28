bce3b00fac5cad1454fb4cb62a1051532126e091
PennyJim <git@pennyjim.com>
2025-08-28 11:40:08 -0600

    Add option for recursive search

--- mp3_lyrics/__main__.py
+++ mp3_lyrics/__main__.py
@@ -8,6 +8,9 @@ from progress.bar import Bar
 parser = ArgumentParser()
 parser.add_argument("-i", "--input", dest="inputdir", required=True,
                     help="use audio files inside the directory DIR (Required)", metavar="DIR")
+parser.add_argument("-r", "--recursive",
+                    action="store_true", dest="recursive", default=False,
+                    help="Whether it should recursively check in directories for more mp3 files")
 parser.add_argument("--website", dest="website", default="auto",
                     help="use specific website (azlyrics, genius, darklyrics)", metavar="WEBSITE")
 parser.add_argument("--overwrite",
@@ -26,6 +29,7 @@ LOG_FILE           = args.logfile
 OVERWRITE_ORIGINAL = args.overwrite
 WEBSITE            = args.website
 IN_DIRECTORY       = args.inputdir
+IS_RECURSIVE       = args.recursive
 
 if not IN_DIRECTORY.endswith("/"):
     IN_DIRECTORY = IN_DIRECTORY+"/"
@@ -38,7 +42,7 @@ else:
 
 # effectively the working directory from now on
 directory = OUT_DIRECTORY
-mp3_files = os_tools.find_mp3_files(directory)
+mp3_files = os_tools.find_mp3_files(directory, IS_RECURSIVE)
 
 if SHOW_PROGRESS:
     bar = Bar('Progress', max=len(mp3_files))
@@ -46,12 +50,11 @@ if SHOW_PROGRESS:
 for mp3_file in mp3_files:
     if SHOW_PROGRESS:
         bar.next()
-    file_path = directory + mp3_file
-    if id3_tools.is_lyrics_tag_present(file_path):
+    if id3_tools.is_lyrics_tag_present(mp3_file):
         # This file already has embedded lyrics. Skipping the file.
         continue
 
-    band_name, song_name = id3_tools.get_song_details(file_path)
+    band_name, song_name = id3_tools.get_song_details(mp3_file)
 
     try:
         lyrics = azlyrics.get_lyrics(band_name, song_name)
@@ -59,7 +62,7 @@ for mp3_file in mp3_files:
         # Error while downloading. Skipping this song.
         continue
 
-    id3_tools.embed_lyrics(file_path, lyrics)
+    id3_tools.embed_lyrics(mp3_file, lyrics)
 
 if SHOW_PROGRESS:    
     bar.finish()
--- mp3_lyrics/os_tools.py
+++ mp3_lyrics/os_tools.py
@@ -1,12 +1,26 @@
-import os
+from os import scandir
 import shutil
 
-def find_mp3_files(directory):
+def scantree(path):
+    """Recursively yield DirEntry objects for given directory."""
+    for entry in scandir(path):
+        if entry.is_dir(follow_symlinks=False):
+            yield from scantree(entry.path)
+        else:
+            yield entry
+
+def find_mp3_files(directory, is_recursive=True):
     files = []
 
-    for file in os.listdir(directory):
-        if file.endswith(".mp3"):
-            files.append(file)
+    if is_recursive:
+        for file in scantree(directory):
+            if file.path.endswith(".mp3"):
+                files.append(file.path)
+    else:
+        with scandir(directory) as entries:
+            for file in entries:
+                if file.path.endswith(".mp3"):
+                    files.append(file.path)
     
     files.sort()
     return files
